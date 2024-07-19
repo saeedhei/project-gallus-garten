cdb211df1bc3b4596d6c1d488f0f2cf83547bd60
saeedhei <saeedhei@gmail.com>
2024-07-19 11:14:44 +0200

    ReadME Added

--- src/components/1-home/RightUpfooterView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex flex-col md:flex-row">
-    <div class="w-1/3">ffffffffffffffff</div>
-    <div class="w-1/3">gggggggggggggggggg</div>
-    <div class="w-1/3">jjjjjjjjjjjjjjjjj</div>
-  </div>
-</template>
